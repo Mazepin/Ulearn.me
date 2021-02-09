4c3bf8526650d0effc216daba12434c486d03e51
Igor <75262127+Mazepin@users.noreply.github.com>
2021-02-09 22:10:05 +0300

    Add project files.

---
+++ Test2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Test2
+{
+    class Program
+    {
+        // имеем Print(GetSquare(42)); нужно дописать 2 метода Print() и GetSquare(), где 42 должно возводиться еще в квадрат
+
+        static void Main(string[] args)
+        {
+            Console.Write("Square number is: ");
+            Print(GetSquare(42));
+
+            Console.ReadKey();
+        }
+
+        static public int GetSquare(int number)
+        {
+            return (int)Math.Pow(number, 2); // возводим в квадрат число
+        }
+        static void Print(int number) => Console.WriteLine(number);
+    }
+}
+
+
+
---
+++ Testing/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Testing
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // округление до целого с выводом на экран имени и зарплаты
+
+            Console.WriteLine(GetGreetingMessage("Student", 10.01));
+            Console.WriteLine(GetGreetingMessage("Bill Gates", 10000000.5));
+            Console.WriteLine(GetGreetingMessage("Steve Jobs", 1));
+            Console.ReadKey();
+        }
+        private static string GetGreetingMessage(string name, double salary)
+        {
+            // возвращает "Hello, <name>, your salary is <salary>"
+            double _salary = (double)Math.Ceiling(salary);
+            Console.WriteLine($"Hello, {name}, your salary is {_salary}");
+            return null;
+        }
+    }
+}
